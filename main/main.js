e973d7757922388e07c64bb7ea13b934e41840cc
Ericdid <ericdidom@gmail.com>
2023-08-23 09:28:40 +0200

    add.js

---
+++ main/main.js
@@ -0,0 +1,7 @@
+// L utente clicca sul bottone e crea una griglia
+const generate = document.getElementById(`Generate`);
+let grid = document.getElementById(`grid`);
+
+button = addEventListener("click", function () {
+  generateGrid();
+});
